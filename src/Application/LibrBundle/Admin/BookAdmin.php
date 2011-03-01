eb14df3f3a44bd25084c8d4b990aa6c806107de6
Wojciech Sznapka <wojtek@sznapka.pl>
2011-03-01 17:33:29 +0100

    Base Application changes

--- src/Application/LibrBundle/Admin/BookAdmin.php
+++ src/Application/LibrBundle/Admin/BookAdmin.php
@@ -5,9 +5,4 @@ use Sonata\BaseApplicationBundle\Admin\EntityAdmin;
 class BookAdmin extends EntityAdmin
 {
   public $baseRouteName = "libr_book";
-  protected $list = array("title" => array("identifier" => true), "author");
-  protected $filter = array("title", "author");
-  protected $form = array("title", "author");
-  protected $label = "Książki";
-  protected $group = "xxxxx";
 }
