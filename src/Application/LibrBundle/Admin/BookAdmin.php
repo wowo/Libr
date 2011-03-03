858746600b6e3a859f138dd2d2db07bae9ff7b7d
Wojciech Sznapka <wojtek@sznapka.pl>
2011-03-03 17:29:31 +0100

    Removed options from Admin class and moved them to DI

--- src/Application/LibrBundle/Admin/BookAdmin.php
+++ src/Application/LibrBundle/Admin/BookAdmin.php
@@ -4,7 +4,5 @@ use Sonata\AdminBundle\Admin\EntityAdmin;
 
 class BookAdmin extends EntityAdmin
 {
-  public $baseRouteName = "libr_book";
-  protected $formOptions = array("validation_groups" => "Default");
 
 }
