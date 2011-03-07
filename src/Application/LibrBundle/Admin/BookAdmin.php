f718ee5ac1063877ea963697a5d1c6df8a572cd4
Wojciech Sznapka <wojtek@sznapka.pl>
2011-03-07 12:37:16 +0100

    Admin Bundle changes

--- src/Application/LibrBundle/Admin/BookAdmin.php
+++ src/Application/LibrBundle/Admin/BookAdmin.php
@@ -1,8 +1,11 @@
 <?php
 namespace Application\LibrBundle\Admin;
-use Sonata\AdminBundle\Admin\EntityAdmin;
+use Sonata\AdminBundle\Admin\Admin;
 
-class BookAdmin extends EntityAdmin
+class BookAdmin extends Admin
 {
-
+  protected $list = array("title" => array("identifier" => true), "author");
+  protected $filter = array("title", "author");
+  protected $form = array("title", "author");
+  protected $formOptions = array("validation_groups" => "Default");
 }
--- src/Application/LibrBundle/Entity/Book.php
+++ src/Application/LibrBundle/Entity/Book.php
@@ -60,4 +60,3 @@ class Book
     return $this->id;
   }
 }
-
